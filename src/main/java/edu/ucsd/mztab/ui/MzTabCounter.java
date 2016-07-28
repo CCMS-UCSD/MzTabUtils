4c3f35058267fa8250e54104a360115a52ffa16a
jjcarver <jjcarver@ucsd.edu>
2016-07-28 08:50:41 -0700

    Added code to MzTabCounter to parse global FDR values from the relevant MTD row to write into the summary file.

--- src/main/java/edu/ucsd/mztab/ui/MzTabCounter.java
+++ src/main/java/edu/ucsd/mztab/ui/MzTabCounter.java
@@ -1,16 +1,21 @@
 package edu.ucsd.mztab.ui;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
 
 import org.apache.commons.io.FilenameUtils;
 
 import edu.ucsd.mztab.MzTabReader;
 import edu.ucsd.mztab.TaskMzTabContext;
+import edu.ucsd.mztab.model.MzTabConstants;
 import edu.ucsd.mztab.model.MzTabFile;
 import edu.ucsd.mztab.processors.CountProcessor;
 
@@ -45,11 +50,14 @@ public class MzTabCounter
 			writer = new PrintWriter(new BufferedWriter(
 				new FileWriter(count.outputFile, false)));
 			writer.println(
-				"MzTab_file\tResult_file\tPSM_rows\tFound_PSMs\t" +
-				"PEP_rows\tFound_Peptides\tPRT_rows\tFound_Proteins\t" +
-				"Found_Mods");
+				"MzTab_file\tResult_file\tPSM_rows\tFound_PSMs\tPSM_FDR\t" +
+				"PEP_rows\tFound_Peptides\tPeptide_FDR\t" +
+				"PRT_rows\tFound_Proteins\tProtein_FDR\tFound_Mods");
 			// read through all mzTab files, write counts to output file
-			for (File file : count.mzTabDirectory.listFiles()) {
+			File[] files = count.mzTabDirectory.listFiles();
+			// sort files alphabetically
+			Arrays.sort(files);
+			for (File file : files) {
 				Map<String, Integer> counts = new HashMap<String, Integer>(7);
 				MzTabFile mzTabFile = context.getMzTabFile(file);
 				MzTabReader reader = new MzTabReader(mzTabFile);
@@ -60,12 +68,14 @@ public class MzTabCounter
 					mzTabFile.getUploadedResultPath());
 				if (uploadedFilename == null)
 					uploadedFilename = mzTabFile.getMzTabFilename();
+				// extract global FDR values to print to output file
+				String[] fdr = extractGlobalFDRValues(file);
 				writer.println(String.format(
-					"%s\t%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d",
+					"%s\t%s\t%d\t%d\t%s\t%d\t%d\t%s\t%d\t%d\t%s\t%d",
 					file.getName(), uploadedFilename,
-					counts.get("PSM"), counts.get("PSM_ID"),
-					counts.get("PEP"), counts.get("sequence"),
-					counts.get("PRT"), counts.get("accession"),
+					counts.get("PSM"), counts.get("PSM_ID"), fdr[0],
+					counts.get("PEP"), counts.get("sequence"), fdr[1],
+					counts.get("PRT"), counts.get("accession"), fdr[2],
 					counts.get("modification")));
 			}
 		} catch (Throwable error) {
@@ -162,6 +172,85 @@ public class MzTabCounter
 			System.err.println(error.getMessage());
 			return null;
 		}
+	}
+	
+	private static String[] extractGlobalFDRValues(File mzTabFile) {
+		if (mzTabFile == null || mzTabFile.isFile() == false ||
+			mzTabFile.canRead() == false)
+			return null;
+		// read through input mzTab file, pull out global FDR values
+		// from the "false_discovery_rate" MTD row, if present
+		BufferedReader reader = null;
+		String[] fdr = new String[3];
+		try {
+			reader = new BufferedReader(new FileReader(mzTabFile));
+			String line = null;
+			while (true) {
+				line = reader.readLine();
+				if (line == null)
+					break;
+				// if the next line is a comment or is blank, skip it
+				if (line.startsWith("COM") || line.trim().equals(""))
+					continue;
+				// if the next line is past the MTD section, then we're done
+				else if (line.startsWith("MTD") == false)
+					break;
+				else {
+					// if this is not the FDR line, skip it
+					Matcher matcher =
+						MzTabConstants.FDR_LINE_PATTERN.matcher(line);
+					if (matcher.matches() == false)
+						continue;
+					// parse out FDR values from the FDR line and validate them
+					String fdrList = matcher.group(1);
+					if (fdrList == null)
+						return fdr;
+					fdrList = fdrList.trim();
+					if (fdrList.equals("") || fdrList.equalsIgnoreCase("null"))
+						return fdr;
+					String[] fdrCVTerms = fdrList.split("\\|");
+					if (fdrCVTerms == null || fdrCVTerms.length < 1)
+						return fdr;
+					for (String fdrCVTerm : fdrCVTerms) {
+						matcher =
+							MzTabConstants.CV_TERM_PATTERN.matcher(fdrCVTerm);
+						if (matcher.matches()) {
+							// parse out the CV term and match it up to the
+							// correct FDR index in the returned array
+							String accession = matcher.group(2);
+							if (accession == null)
+								continue;
+							accession = accession.trim();
+							Integer index = null;
+							// PSM-level FDR
+							if (accession.equals("MS:1002350"))
+								index = 0;
+							// Peptide-level FDR
+							else if (accession.equals("MS:1001364"))
+								index = 1;
+							// Protein-level FDR
+							else if (accession.equals("MS:1001214"))
+								index = 2;
+							// some other CV term
+							else continue;
+							// ensure CV term value parses to double
+							String value = matcher.group(4);
+							try { Double.parseDouble(value); }
+							catch (Throwable error) { continue; }
+							// write validated CV term value to correct slot
+							fdr[index] = value;
+						}
+					}
+				}
+			}
+		} catch (RuntimeException error) {
+			throw error;
+		} catch (Throwable error) {
+			throw new RuntimeException(error);
+		} finally {
+			try { reader.close(); } catch (Throwable error) {}
+		}
+		return fdr;
 	}
 	
 	private static void die(String message) {
