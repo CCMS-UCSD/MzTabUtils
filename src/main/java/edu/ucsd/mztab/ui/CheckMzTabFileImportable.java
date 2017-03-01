c84099e49c9a360b427c8066280b43d5480c7fec
jjcarver <jjcarver@ucsd.edu>
2017-03-01 14:01:06 -0800

    Added a UI class to check if an mzTab file is importable to MassIVE search.

---
+++ src/main/java/edu/ucsd/mztab/ui/CheckMzTabFileImportable.java
@@ -0,0 +1,59 @@
+package edu.ucsd.mztab.ui;
+
+import java.io.File;
+
+public class CheckMzTabFileImportable
+{
+	/*========================================================================
+	 * Constants
+	 *========================================================================*/
+	private static final String USAGE =
+		"java -cp MzTabUtils.jar edu.ucsd.mztab.ui.CheckMzTabFileImportable " +
+		"<MzTabFile>";
+	
+	/*========================================================================
+	 * Public interface methods
+	 *========================================================================*/
+	public static void main(String[] args) {
+		if (args == null || args.length != 1)
+			die(USAGE);
+		File mzTabFile = new File(args[0]);
+		try {
+			if (MzTabPROXIImporter.isImportable(mzTabFile))
+				System.out.println(String.format(
+					"MzTab file [%s] is importable to MassIVE search.",
+					mzTabFile.getName()));
+			else System.out.println(String.format(
+				"Argument [%s] does not correspond to an mzTab file that " +
+				"is importable to MassIVE search.", mzTabFile.getName()));
+		} catch (Throwable error) {
+			die(null, error);
+		}
+	}
+	
+	/*========================================================================
+	 * Convenience methods
+	 *========================================================================*/
+	private static void die(String message) {
+		die(message, null);
+	}
+	
+	private static void die(String message, Throwable error) {
+		if (message == null)
+			message = "There was an error parsing an mzTab file to determine " +
+				"whether or not it is importable to MassIVE search";
+		if (error == null) {
+			if (message.endsWith(".") == false)
+				message += ".";
+		} else {
+			if (message.endsWith("."))
+				message = message.substring(0, message.length() - 1);
+			if (message.endsWith(":") == false)
+				message += ":";
+		}
+		System.err.println(message);
+		if (error != null)
+			error.printStackTrace();
+		System.exit(1);
+	}
+}
